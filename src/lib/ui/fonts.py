aea46f151a9746c159f53dbfc0c39e55f9c574da
return-p <110697125+return-p@users.noreply.github.com>
2023-09-15 22:19:49 +0900

    Add files via upload

--- src/lib/ui/fonts.py
+++ src/lib/ui/fonts.py
@@ -4,23 +4,23 @@ import pygame
 pygame.font.init()
 
 FONT_PRIMARY_SMALL = pygame.font.Font(
-    path.join('assets', 'VT323-Regular.ttf'), 14)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 14)
 FONT_PRIMARY_MEDIUM = pygame.font.Font(
-    path.join('assets', 'VT323-Regular.ttf'), 20)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 20)
 FONT_PRIMARY_LARGE = pygame.font.Font(
-    path.join('assets', 'VT323-Regular.ttf'), 24)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 24)
 FONT_PRIMARY_XLARGE = pygame.font.Font(
-    path.join('assets', 'VT323-Regular.ttf'), 30)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 30)
 FONT_PRIMARY_XXLARGE = pygame.font.Font(
-    path.join('assets', 'VT323-Regular.ttf'), 40)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 40)
 
 FONT_SECONDARY_XXXSMALL = pygame.font.Font(
-    path.join('assets', 'VictorMono-Regular.ttf'), 8)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 8)
 FONT_SECONDARY_XXSMALL = pygame.font.Font(
-    path.join('assets', 'VictorMono-Regular.ttf'), 9)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 9)
 FONT_SECONDARY_XSMALL = pygame.font.Font(
-    path.join('assets', 'VictorMono-Regular.ttf'), 12)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 12)
 FONT_SECONDARY_SMALL = pygame.font.Font(
-    path.join('assets', 'VictorMono-Regular.ttf'), 14)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 14)
 FONT_SECONDARY_MEDIUM = pygame.font.Font(
-    path.join('assets', 'VictorMono-Regular.ttf'), 20)
+    path.join('assets', '경기천년바탕_Bold.ttf'), 20)
